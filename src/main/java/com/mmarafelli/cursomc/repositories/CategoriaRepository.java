5c33f5b84b92db14d44211cbf2ab3aa7c3f9060d
Matheus Marafelli <mmarafelli@outlook.com>
2018-09-06 20:41:55 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mmarafelli/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.mmarafelli.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mmarafelli.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
