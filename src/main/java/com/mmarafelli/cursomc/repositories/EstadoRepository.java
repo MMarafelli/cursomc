0fd91787129186eaf8581b25a5c03dae7609fbc5
Matheus Marafelli <mmarafelli@outlook.com>
2018-09-09 23:25:08 -0300

    Estado e cidade

---
+++ src/main/java/com/mmarafelli/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.mmarafelli.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mmarafelli.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
