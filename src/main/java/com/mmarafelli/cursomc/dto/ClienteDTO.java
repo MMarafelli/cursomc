0f1852495c4feb0a0f5290eeacde1a6195226846
Matheus Marafelli <mmarafelli@outlook.com>
2018-11-04 15:06:29 -0200

    validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/mmarafelli/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/mmarafelli/cursomc/dto/ClienteDTO.java
@@ -1,11 +1,16 @@
 package com.mmarafelli.cursomc.dto;
 
 import java.io.Serializable;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.Length;
-import com.mmarafelli.cursomc.domain.Cliente;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.mmarafelli.cursomc.domain.Cliente;
+import com.mmarafelli.cursomc.services.validation.ClienteUpdate;
+
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
